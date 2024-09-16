6e70abbf7c52ebfd6904bca5548fea3f15e028d6
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-09-16 09:40:02 +0300

    rm excess

--- examples/no_api_key.py
+++
@@ -1,8 +0,0 @@
-import binance
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-client = binance.Client()
-r = client.get_historical_klines('BTCUSDT', client.KLINE_INTERVAL_1DAY, '1-Dec-2017', '10-Dec-2017')
-for rr in r:
-    print(rr)
